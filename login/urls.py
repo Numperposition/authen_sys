a9114acbc9eac58d2fa4f93dac790489bdbbd3f8
Numperposition <32921945+Numperposition@users.noreply.github.com>
2020-03-07 20:01:18 +0100

    certificate - backup

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,13 @@
 
 from django.urls import path, include
-from login.views import test, handle_redirect, send_code
+from login.views import test, handle_redirect, send_req_to_gw, redirect_to_login, cert_upload
 
 
 urlpatterns = [
+    path(r'', redirect_to_login),
     path(r'login/', test),
+    path(r'send_request/', send_req_to_gw),
     path(r'second_authen/', handle_redirect),
+    path(r'cert_upload/', cert_upload),
 
 ]
--- login/views.py
+++ login/views.py
@@ -4,9 +4,49 @@ from django.contrib.auth import authenticate, login, logout
 from login.models import userInfo
 from django.core.mail import send_mail, send_mass_mail, EmailMultiAlternatives
 from django.conf import settings
-import random
+import random, socket, sys, json
+from login.cert_analyzer import cert_analysis
 
 # Create your views here.
+
+def cert_upload(request):
+    user = request.user
+    if request.method == 'GET':
+        if user.is_authenticated is False:
+            return render(request, 'login/file_upload.html')
+        else:
+            return HttpResponse("You have alreadly login!")
+    elif request.method == 'POST':
+        file_obj = request.FILES.get('file', None)
+        if file_obj == None:
+            return render(request, 'login/file_upload.html')
+        str = settings.BASE_DIR + "\\download\\" + file_obj.name
+        with open(str, 'wb') as f:
+            for line in file_obj.chunks():
+                f.write(line)
+        f.close()
+        if cert_analysis(str) == True:
+            username = request.POST['username']
+            password = request.POST['password']
+            try:
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    #return HttpResponseRedirect('/send_request')
+                    return HttpResponse('login successfully')
+                # return HttpResponse('login success.')
+                else:
+                    return HttpResponse('login fail')
+            except:
+                return render(request, 'login/file_upload.html')
+        return HttpResponse("login fail")
+
+
+
+
+def redirect_to_login(request):
+    return HttpResponseRedirect('/login')
+
 def handle_redirect(request):
     user = request.user
     if user.is_authenticated is True:
@@ -21,15 +61,47 @@ def handle_redirect(request):
 
 
 
-def send_code(request):
+def send_req_to_gw(request):
+    user = request.user
+    if user.is_authenticated is True:
+        username = user.username
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(('192.168.1.1', 9906))  # router IP
+        except socket.error as msg:
+            print(msg)
+            print(sys.exit(1))
+        if "HTTP_X_FORWARDED_FOR" in request.META:
+            ip = request.META['HTTP_X_FORWARDED_FOR']
+        else:
+            ip = request.META['REMOTE_ADDR']
+        data = {"ip": ip, "user": username}
+        data = json.dumps(data)  # convert dict object to json string
 
-    pass
+        s.send(data.encode())
+        print(data)
+        msg = s.recv(1024)
+        msg = msg.decode()
+        dict_msg = json.loads(msg)
+        print(msg)
+        status = dict_msg['status']
+        if msg is not None:
+            s.close()
+        context = {'username': username, 'status': status}
+        return render(request, 'login/send_req.html', context)
+    else:
+        return HttpResponseRedirect('/login')
 
 def test(request):
     # return HttpResponse('login page test.')
     if request.method == 'GET':
         context = {}
         context['previous_page'] = request.GET.get('from_page')
+        if "HTTP_X_FORWARDED_FOR" in request.META:
+            ip = request.META['HTTP_X_FORWARDED_FOR']
+        else:
+            ip = request.META['REMOTE_ADDR']
+        print("user ip = ", ip)
         return render(request, 'login/login.html', context)
     else:
         if 'submit_code' in request.POST:
@@ -61,8 +133,8 @@ def test(request):
                 user = authenticate(request, username=username, password=password)
                 if user is not None and token == user_token:
                     login(request, user)
-                    #return HttpResponseRedirect('/second_authen')
-                    return HttpResponse('login successfully')
+                    return HttpResponseRedirect('/send_request')
+                    #return HttpResponse('login successfully')
                 # return HttpResponse('login success.')
                 else:
                     return HttpResponse('login fail')
